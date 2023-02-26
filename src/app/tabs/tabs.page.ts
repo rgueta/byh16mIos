d90ddbcfe95eebbde9856f3de1085d36d99221e8
Gueta <ricardogueta@hotmail.com>
2023-02-26 12:08:28 -0800

    first commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
+  SoyAdmin = localStorage.getItem('IsAdmin');
+  myRoles:any;
   constructor() {}
 
 }
